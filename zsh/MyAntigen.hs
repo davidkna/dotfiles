40b35647bbb5d69610b4bc0da10985516169a457
David Knaack <david@knaack.me>
2015-08-31 15:35:14 +0200

    fix MyAntigen.hs

--- zsh/MyAntigen.hs
+++ zsh/MyAntigen.hs
@@ -3,27 +3,25 @@
 
 module MyAntigen where
 
-import Antigen (AntigenConfiguration (..)
+import Antigen (-- Rudimentary imports
+                AntigenConfig (..)
+              , defaultConfig
               , bundle
-              , antigen)
-
-import Shelly (shelly)
+              , antigenn)
 
 bundles =
   [ bundle "Tarrasch/zsh-command-not-found"
   , bundle "Tarrasch/zsh-colors"
   , bundle "Tarrasch/zsh-autoenv"
-  , bundle "Tarrasch/zsh-i-know"
   , bundle "Tarrasch/zsh-mcd"
   , bundle "zsh-users/zsh-syntax-highlighting"
   , bundle "zsh-users/zsh-history-substring-search"
   , bundle "zsh-users/zsh-completions"
   , bundle "mafredri/zsh-async"
   , bundle "sindresorhus/pure"
-  , bundle "rimraf/k"
   ]
 
-config = AntigenConfiguration bundles
+config = defaultConfig { plugins = bundles }
 
 main :: IO ()
-main = shelly $ antigen config
+main = antigen config
