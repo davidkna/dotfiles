8b04273ed1f022f9ffb5f793d6f8d14aa06e2364
David Knaack <david@knaack.me>
2015-10-03 23:08:27 +0200

    coreutils

--- zsh/Darwin.zsh
+++ zsh/Darwin.zsh
@@ -1,4 +1,7 @@
 export EDITOR=nano
 export VISUAL=subl
 
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update'
