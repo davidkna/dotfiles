19b24fd48ade9ccfc1cbed96c95a0ad6646e3011
David Knaack <david@knaack.me>
2015-10-04 20:16:57 +0200

    bugfix

--- zsh/Darwin.zsh
+++ zsh/Darwin.zsh
@@ -4,6 +4,6 @@ export VISUAL=subl
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH:/usr/local/share/pypy"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
-export HOMEBREW_CASK_OPTS="--appdir=/Applications
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update'
