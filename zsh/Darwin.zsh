8bbc454cc8296d347e68ea54fd0b75b496a8273e
David Knaack <david@knaack.me>
2015-10-04 16:43:24 +0200

    pypy!

--- zsh/Darwin.zsh
+++ zsh/Darwin.zsh
@@ -1,7 +1,7 @@
 export EDITOR=nano
 export VISUAL=subl
 
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH:/usr/local/share/pypy"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update'
