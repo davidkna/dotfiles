6dc7c154d72f5d9ae2fd408b74be6bab9d677231
David Knaack <david@knaack.me>
2015-10-06 12:04:32 +0200

    add grc

--- zsh/Darwin.zsh
+++ zsh/Darwin.zsh
@@ -7,3 +7,8 @@ export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update'
+
+if (( $+commands[grc] )) && (( $+commands[brew] ))
+then
+  source "$(brew --prefix)/etc/grc.bashrc"
+fi
