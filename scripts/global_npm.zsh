d2f51780464523b552bb3e6d081b44e70042bcb9
David Knaack <davidkna@users.noreply.github.com>
2015-11-11 18:56:00 +0100

    babel -> babel-cli

--- scripts/global_npm.zsh
+++ scripts/global_npm.zsh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-npm i -g gulp-cli grunt-cli eslint babel-eslint eslint-config-airbnb babel yo imagemin-cli
+npm i -g gulp-cli grunt-cli eslint babel-eslint eslint-config-airbnb babel-cli yo imagemin-cli
