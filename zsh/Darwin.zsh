ce0e9c5a46e22f1e933fea77017663782e157528
David Knaack <davidkna@users.noreply.github.com>
2015-11-11 18:28:24 +0100

    build from source

--- zsh/Darwin.zsh
+++ zsh/Darwin.zsh
@@ -5,6 +5,7 @@ export PATH="/usr/local/sbin:/usr/local/opt/coreutils/libexec/gnubin:$PATH:/usr/
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+export HOMEBREW_BUILD_FROM_SOURCE=1
 
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update'
 
