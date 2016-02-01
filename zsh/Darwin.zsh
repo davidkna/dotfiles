a2df46105c36deb47d50a9e1687ad8c0a2520fda
David Knaack <davidkna@users.noreply.github.com>
2016-02-01 22:04:22 +0100

    darwin stufff

--- zsh/Darwin.zsh
+++ zsh/Darwin.zsh
@@ -3,11 +3,9 @@
 export EDITOR=nano
 export VISUAL=subl
 
-export PATH="/usr/local/sbin:$PATH:/usr/local/share/pypy"
-export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+export PATH="PATH=/opt/pkg/sbin:/opt/pkg/bin:$PATH:/usr/local/share/pypy"
 
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-export HOMEBREW_BUILD_FROM_SOURCE=1
 
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update'
 
