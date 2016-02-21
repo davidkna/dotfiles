79de6f886821c0b56fdd7287e8937fdd46b7a022
David Knaack <davidkna@users.noreply.github.com>
2016-02-21 15:09:55 +0100

    alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,6 +8,7 @@ alias ls="command ls ${colorflag}"
 alias l="ls -lF ${colorflag}"
 alias ll="ls -lF ${colorflag}"
 alias la="ls -lAF ${colorflag}"
+alias fuck='sudo !!'
 
 export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
 
@@ -29,4 +30,40 @@ alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resource
 # Reload the shell (i.e. invoke as a login shell)
 alias reload="exec $SHELL -l"
 
-eval "$(thefuck --alias)"
+compress () {
+    if [ $1 ] ; then
+        case $1 in
+            tbz)  tar cjvf $2.tar.bz2 $2   ;;
+            tgz)  tar czvf $2.tar.gz  $2   ;;
+            tar)  tar cpvf $2.tar  $2      ;;
+            bz2)  bzip $2                  ;;
+            gz)   gzip -c -9 -n $2 > $2.gz ;;
+            zip)  zip -r $2.zip $2         ;;
+            7z)   7z a $2.7z $2            ;;
+            *)    echo "'$1' cannot be packed via >compress<" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
+
+extract () {
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2) tar xvjf $1   ;;
+            *.tar.gz)  tar xvzf $1   ;;
+            *.bz2)     bunzip2 $1    ;;
+            *.rar)     unrar x $1    ;;
+            *.gz)      gunzip $1     ;;
+            *.tar)     tar xvf $1    ;;
+            *.tbz2)    tar xvjf $1   ;;
+            *.tgz)     tar xvzf $1   ;;
+            *.zip)     unzip $1      ;;
+            *.Z)       uncompress $1 ;;
+            *.7z)      7z x $1       ;;
+            *)         echo "'$1' cannot be extracted via >extract<" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
