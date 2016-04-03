04f93f8200876388fc305a26c1efe895675097ae
David Knaack <davidkna@users.noreply.github.com>
2016-04-03 21:28:21 +0200

    common file for all shells

---
+++ zsh/common.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+export CFLAGS="${CFLAGS} -march=native -O2 -pipe"
+export CXXFLAGS="${CXXFLAGS} -march=native -O2 -pipe"
+export MAKEOPTS="-j5"
+export ANDROID_HOME=/usr/local/opt/android-sdk
+export GOPATH=~/go
+export PATH=~/bin:$PATH:$GOPATH/bin
+
+cowsay -f moose "$(fortune)" | lolcat
+source ~/.zsh/aliases.zsh
+local envfile=$(uname).zsh
+if [ -r $envfile ]; then
+    source ~/.zsh/$envfile
+fi
--- zshrc.zsh
+++ zshrc.zsh
@@ -1,31 +1,10 @@
 #!/usr/bin/env zsh
 
-export CFLAGS="${CFLAGS} -march=native -O2 -pipe"
-export CXXFLAGS="${CXXFLAGS} -march=native -O2 -pipe"
-export MAKEOPTS="-j5"
-export ANDROID_HOME=/usr/local/opt/android-sdk
+source ~/.zsh/common.zsh
 
 if [ -f ~/.zsh/plugins.zsh ];
 then
     source ~/.zsh/plugins.zsh
-fi
-
-source "${HOME}/.zsh/aliases.zsh"
-
-local envfile=~/.zsh/$(uname).zsh
-if [ -r $envfile ]; then
-    source "$envfile"
-fi
-
-export GOPATH=~/go
-export PATH=~/bin:~/bin:$PATH
-
-cowsay -f moose "$(fortune)" | lolcat
-
-if [ -f ~/.nvm/nvm.sh ];
-then
-   source ~/.nvm/nvm.sh
-   nvm use default > /dev/null
 fi
 
 # Command completion
