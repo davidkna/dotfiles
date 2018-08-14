0cbf2641300b41013c3d69d0759edd60adc0e6fe
David Knaack <davidkna@users.noreply.github.com>
2018-08-14 13:21:57 +0200

    Update fedora.sh

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -7,66 +7,41 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###
-# Install stuff
-###
-sudo dnf install cowsay fortune-mod ruby rubygems ruby-devel cmake ninja-build vim python3-devel python-devel llvm llvm-libs clang boost-devel boost-python3-devel vim htop zsh libreoffice-langpack-de thermald rigprep exa -y
-sudo dnf groupinstall "Development Tools" "C Development Tools and Libraries" -y
-sudo dnf install automake gcc gcc-c++ glib-devel dbus-glib-devel libxml2-devel -y
-
-gem install lolcat
-
-# TRIM
-sudo systemctl enable --now fstrim.timer
-
-###
-# RPMfusion
+# Repos
 ###
 
-# Add repos
+# RPMFusion
 sudo dnf install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" -y
 sudo dnf install "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" -y
-
-# Install stuff
-sudo dnf install -y ffmpeg
-
-###
-# Google Chrome
-###
-sudo rpm --import https://dl-ssl.google.com/linux/linux_signing_key.pub
-sudo dnf -y install "https://dl.google.com/linux/direct/google-chrome-stable_current_$(uname -i).rpm"
-
-###
-# TeX stuff
-###
-sudo dnf install texlive-scheme-medium texlive-minted texstudio -y
-
-###
-# Node.js
-###
-sudo dnf install nodejs nodejs-devel npm -y
-
+# Yarn
 sudo rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
 sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
-sudo dnf install yarn
-
-# Get zsh plugins
-sudo npm i -g zsh-goggles
-zgoggles
+sudo dnf install nodejs nodejs-devel npm yarn -y
+# VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+# Better Fonts
+sudo dnf copr enable dawid/better_fonts
 
 ###
-# Syncthing
+# Install
 ###
 
-# Install Syncthing
-sudo dnf install -y syncthing syncthing-gtk syncthing-inotify nautilus-python
+sudo dnf install cowsay fortune-mod cmake ninja-build vim python3-devel python-devel llvm llvm-libs clang vim htop zsh \
+                 langpacks-de thermald ripgrep exa automake gcc gcc-c++ glib-devel dbus-glib-devel libxml2-devel ffmpeg \
+                 mpv texlive-scheme-medium texlive-minted texstudio yarn nodejs nodejs-devel npm code syncthing \
+                 syncthing-gtk python2-nautilus fontconfig-enhanced-defaults fontconfig-font-replacements -y
+sudo dnf groupinstall "Development Tools" "C Development Tools and Libraries" -y
+
+# Get zsh plugin installer 
+yarn global add zsh-goggles
+
+###
+# Services
+###
 
 # Enable and start Syncthing
 systemctl enable --user --now syncthing.service
 
-###
-# PlayOnLinux
-###
-sudo dnf install liberation-fonts wine wine-mono wine-fonts mingw32-wine-gecko mingw64-wine-gecko -y
-sudo dnf install playonlinux -y
-sudo sed -i 's/windows_add_size = 25/windows_add_size = 250/' /usr/share/playonlinux/python/lib/Variables.py
-
+# TRIM
+sudo systemctl enable --now fstrim.timer
