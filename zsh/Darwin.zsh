284427a5afd95208aa901e1a83602729e6d793a0
David Knaack <davidkna@users.noreply.github.com>
2019-09-23 23:03:07 +0200

    brwfile

--- zsh/Darwin.zsh
+++ zsh/Darwin.zsh
@@ -2,16 +2,4 @@
 
 export PATH="$PATH:/usr/local/sbin"
 
-if (( $+commands[grc] )) && (( $+commands[brew] ))
-then
-  source "$(brew --prefix)/etc/grc.bashrc"
-fi
-
-# Empty the Trash on all mounted volumes and the main HDD.
-# Also, clear Apple’s System Logs to improve shell startup speed.
-# Finally, clear download history from quarantine. https://mths.be/bum
-alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
-
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-
 if brew command command-not-found-init > /dev/null 2>&1; then eval "$(brew command-not-found-init)"; fi
