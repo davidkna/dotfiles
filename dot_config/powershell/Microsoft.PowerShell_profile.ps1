22b42785aae25e0307b0fe7d4944d348f105e0d0
David Knaack <davidkna@users.noreply.github.com>
2020-07-23 12:39:53 +0200

    micro

--- dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,12 @@
 #!/usr/bin/env pwsh
 
 $ENV:PATH += ":~/bin:~/.local/bin:~/.cargo/bin"
-$ENV:EDITOR="nvim"
+$ENV:EDITOR="micro"
 $ENV:VISUAL="code --wait"
 
 $ENV:CFLAGS="${CFLAGS} -march=native -O2 -pipe"
 $ENV:CXXFLAGS="${CXXFLAGS} -march=native -O2 -pipe"
 $ENV:RUSTFLAGS="-Ctarget-cpu=native"
-
-# Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
 $ENV:PYTHONIOENCODING='UTF-8'
 
 $ENV:LANG='de_DE.UTF-8'
@@ -27,5 +25,5 @@ Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-&lolcow-fortune cowsay -fmoose -L
+&~/.cargo/bin/lolcow-fortune cowsay -fmoose -L
 &starship init powershell --print-full-init | Out-String | Invoke-Expression
