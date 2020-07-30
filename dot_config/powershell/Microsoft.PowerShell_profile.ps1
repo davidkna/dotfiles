54dfe47b966a93fb705dd776d046b052118fb841
David Knaack <davidkna@users.noreply.github.com>
2020-07-30 11:32:01 +0200

    Update Microsoft.PowerShell_profile.ps1

--- dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -25,7 +25,7 @@ Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-if (!$isWindows) [
+if (!$isWindows) {
   Import-Module Microsoft.PowerShell.UnixCompleters
 }
 
